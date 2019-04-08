b931cc58776292184925af0ca4f9eaf47affd6dd
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-08 16:15:23 +0300

    Extracting ProductCard component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
-import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/switchMap';
@@ -13,13 +12,11 @@ import 'rxjs/add/operator/switchMap';
 export class ProductsComponent {
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  categories$;
   category: string;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService, 
-    categoryService: CategoryService) { 
+    productService: ProductService) { 
 
     productService
     .getAll()
@@ -35,8 +32,5 @@ export class ProductsComponent {
         this.products.filter(p => p.category === this.category) :
         this.products;
     });
-
-    this.categories$ = categoryService.getAll();
-
   }
 }
