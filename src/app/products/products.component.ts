245ec4ecf4d146678c3102ce2d755dd8bda9de09
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-10 14:34:48 +0300

    Displaying the quantity

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/switchMap';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
+  cart: any;
+  subscripton: Subscription;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService) { 
-
+    productService: ProductService,
+    private shoppingCartService: ShoppingCartService
+    ) { 
     productService
     .getAll()
     .switchMap(products => {
@@ -33,4 +38,14 @@ export class ProductsComponent {
         this.products;
     });
   }
+
+  async ngOnInit() {
+    this.subscripton = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscripton.unsubscribe();
+  }
+
 }
