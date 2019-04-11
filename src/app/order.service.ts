4e77590a660502110c5c228dcafc35f84f578e8c
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-11 19:45:03 +0300

    Implementing Manage Orders and My Orders

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { ShoppingCartService } from './shopping-cart.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class OrderService {
@@ -13,4 +13,16 @@ export class OrderService {
     return result;
   }
 
+  getOrders() { 
+    return this.db.list('/orders');
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list('/orders', {
+      query: {
+        orderByChild: 'userId',
+        equalTo: userId        
+      }
+    });
+  }
 }
